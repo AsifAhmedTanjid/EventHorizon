fd320d5ae1eb9d4f2ecc951b5b26410890679e58
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-11-27 12:05:56 +0600

    feat:added email service for newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Newsletter() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubscribe = async (e) => {
+    e.preventDefault();
+    if (!email) return toast.error("Please enter an email");
+
+    setLoading(true);
+
+    try {
+      const res = await fetch("/api/subscribe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await res.json();
+
+      if (data.success) {
+        toast.success("Subscribed successfully! Check your email");
+        setEmail("");
+      } else {
+        toast.error("Subscription failed. Try again.");
+      }
+    } catch (err) {
+      toast.error("Something went wrong.");
+    }
+
+    setLoading(false);
+  };
+
   return (
     <section className="py-16  text-white">
-      <div className="max-w-3xl mx-auto px-6 text-center">
+      <div className="max-w-3xl  mx-auto px-6 text-center">
         <h2 className="text-3xl lg:text-4xl font-bold mb-4">
           Stay Updated with EventHorizon
         </h2>
@@ -11,16 +45,20 @@ export default function Newsletter() {
           Subscribe to our newsletter and never miss exciting events happening near you.
         </p>
 
-        <form className="flex flex-col sm:flex-row gap-4 justify-center">
+        <form onSubmit={handleSubscribe} className="flex flex-col sm:flex-row gap-4 justify-center lg:mx-8">
           <input
             type="email"
             placeholder="Enter your email"
-            className="w-full sm:w-auto px-4 py-3 rounded-full text-white focus:outline-none focus:ring-2 focus:ring-blue-500 border"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full sm:w-auto px-4 py-3 rounded-full text-white focus:outline-none focus:ring-2 focus:ring-blue-500 border  md:flex-3"
           />
+
           <button
-            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-full transition hover:cursor-pointer"
+            disabled={loading}
+            className="bg-blue-600 hover:bg-blue-700 disabled:bg-gray-600 text-white font-semibold px-6 py-3 rounded-full transition hover:cursor-pointer md:flex-1"
           >
-            Subscribe
+            {loading ? "Subscribing..." : "Subscribe"}
           </button>
         </form>
 
